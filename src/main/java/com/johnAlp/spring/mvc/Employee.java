b99aaba7afeae977c35cb89c5a8a441bd87acdf9
Evgeny Solomatin <john-alp@mail.ru>
2021-02-04 15:18:28 +0300

    willAddValidation

--- src/main/java/com/johnAlp/spring/mvc/Employee.java
+++ src/main/java/com/johnAlp/spring/mvc/Employee.java
@@ -9,12 +9,27 @@ public class Employee {
     private int salary;
     private String department;
     private Map<String, String> departments;
+    private String carBrand;
+    private Map<String, String> carBrands;
+    private String[] languages;
+    private Map<String, String> languageList;
 
     public Employee() {
         if (departments == null) {
             departments = new HashMap<>();
-            departments.put("","");
+            departments.put("IT","Information Technology");
+            departments.put("HR","Human Resources");
+            departments.put("Sales","Sales");
         }
+        carBrands = new HashMap<>();
+        carBrands.put("BMW", "BMW");
+        carBrands.put("Audi", "Audi");
+        carBrands.put("Mercedes-Benz", "MB");
+
+        languageList = new HashMap<>();
+        languageList.put("English", "EN");
+        languageList.put("Deutch", "DE");
+        languageList.put("French", "FR");
     }
 
     public Employee(String name, String surName, int salary) {
@@ -61,6 +76,38 @@ public class Employee {
 
     public void setDepartments(Map<String, String> departments) {
         this.departments = departments;
+    }
+
+    public String getCarBrand() {
+        return carBrand;
+    }
+
+    public void setCarBrand(String carBrand) {
+        this.carBrand = carBrand;
+    }
+
+    public Map<String, String> getCarBrands() {
+        return carBrands;
+    }
+
+    public void setCarBrands(Map<String, String> carBrands) {
+        this.carBrands = carBrands;
+    }
+
+    public String[] getLanguages() {
+        return languages;
+    }
+
+    public void setLanguages(String[] languages) {
+        this.languages = languages;
+    }
+
+    public Map<String, String> getLanguageList() {
+        return languageList;
+    }
+
+    public void setLanguageList(Map<String, String> languageList) {
+        this.languageList = languageList;
     }
 
     @Override
--- src/main/webapp/WEB-INF/view/ask-emp-details-view.jsp
+++ src/main/webapp/WEB-INF/view/ask-emp-details-view.jsp
@@ -6,7 +6,7 @@
 <h2>Dear Employee, Please enter your details</h2>
 <br><br>
 
-<form:form action = "showDetails" modelAttribute="employee">
+<form:form action="showDetails" modelAttribute="employee">
 
     Name <form:input path="name"/>
     <br><br>
@@ -14,7 +14,32 @@
     <br><br>
     Salary <form:input path="salary"/>
     <br><br>
-<%--здесь сработают сеттеры--%>
+
+    Departament <form:select path="department">
+    <form:options items="${employee.departments}"/>
+    <%--hard code--%>
+    <%--    <form:option value="Information Technology" label="IT"/>--%>
+    <%--    <form:option value="Human Resources" label="HR"/>--%>
+    <%--    <form:option value="Sales" label="Sales"/>--%>
+</form:select>
+    <br><br><br>
+    Which car do you want?
+<form:radiobuttons path="carBrand" items="${employee.carBrands}"/>
+    <%-- hard code--%>
+<%--    BMW <form:radiobutton path="carBrand" value="BMW"/>--%>
+<%--    Audi <form:radiobutton path="carBrand" value="Audi"/>--%>
+<%--    MB <form:radiobutton path="carBrand" value="Mercedes-Benz"/>--%>
+
+    <br><br>
+    Foreign Language(s)
+    <form:checkboxes path="languages" items="${employee.languageList}"/>
+<%--    hard code--%>
+<%--    EN <form:checkbox path="languages" value="English"/>--%>
+<%--    DE <form:checkbox path="languages" value="Deutch"/>--%>
+<%--    FR <form:checkbox path="languages" value="French"/>--%>
+    <br><br>
+
+    <%--здесь сработают сеттеры--%>
     <input type="submit" value="OK!">
 
 </form:form>
